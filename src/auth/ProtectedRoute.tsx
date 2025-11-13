459ac4c678f9c21e6534f3d61e4a874297ac2586
Martin Tsvetanov <martigiant@gmail.com>
2025-11-13 22:20:57 +0100

    Another try

--- src/auth/ProtectedRoute.tsx
+++ src/auth/ProtectedRoute.tsx
@@ -1,14 +1,19 @@
 // src/auth/ProtectedRoute.tsx
-import { Navigate, Outlet, useLocation } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import { useAuth } from "./AuthProvider";
 import Navbar from "../components/Navbar/Navbar";
 
 export function ProtectedRoute() {
     const { loading, isAuthed } = useAuth();
-    const loc = useLocation();
+    const navigation = useNavigate();
     if (loading) return null;
-    if (!isAuthed)
-        return <Navigate to="/login" replace state={{ from: loc }} />;
+
+    if (!isAuthed) {
+        navigation("/");
+        return;
+        // return <Navigate to="/login" replace state={{ from: loc }} />;
+    }
+
     return (
         <>
             <Navbar />
