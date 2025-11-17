ab21fbf376c84b3b8c47f7bb821ddbacacbf7c80
Martin Tsvetanov <martigiant@gmail.com>
2025-11-17 20:24:28 +0100

    try #5

--- src/auth/ProtectedRoute.tsx
+++ src/auth/ProtectedRoute.tsx
@@ -1,17 +1,28 @@
 // src/auth/ProtectedRoute.tsx
-import { Outlet, useNavigate } from "react-router-dom";
+import { Navigate, Outlet, useLocation } from "react-router-dom";
 import { useAuth } from "./AuthProvider";
 import Navbar from "../components/Navbar/Navbar";
+import { useEffect } from "react";
 
 export function ProtectedRoute() {
     const { loading, isAuthed } = useAuth();
-    const navigation = useNavigate();
+    const loc = useLocation();
+
+    console.log(
+        localStorage.getItem("sb-qakickkytstfcbzpyxvw-auth-token"),
+        `localStorage.getItem("sb-qakickkytstfcbzpyxvw-auth-token")`
+    );
+
+    useEffect(() => {
+        if (!localStorage.getItem("sb-qakickkytstfcbzpyxvw-auth-token")) {
+            <Navigate to="/login" replace state={{ from: loc }} />;
+        }
+    }, [loc]);
+
     if (loading) return null;
 
     if (!isAuthed) {
-        navigation("/login");
-        return;
-        // return <Navigate to="/login" replace state={{ from: loc }} />;
+        return <Navigate to="/login" replace state={{ from: loc }} />;
     }
 
     return (
--- src/auth/PublicOnly.tsx
+++ src/auth/PublicOnly.tsx
@@ -1,8 +1,18 @@
-import { Outlet, Navigate } from "react-router-dom";
+import { Outlet, Navigate, useLocation } from "react-router-dom";
 import { useAuth } from "./AuthProvider";
+import { useEffect } from "react";
 
 export function PublicOnly() {
     const { loading, isAuthed } = useAuth();
+    const loc = useLocation();
+    
+    useEffect(() => {
+        if (!localStorage.getItem("sb-qakickkytstfcbzpyxvw-auth-token")) {
+            <Navigate to="/login" replace state={{ from: loc }} />;
+        }
+    }, [loc]);
+
     if (loading) return null; // or a spinner
+
     return isAuthed ? <Navigate to="/dashboard" /> : <Outlet />;
 }
