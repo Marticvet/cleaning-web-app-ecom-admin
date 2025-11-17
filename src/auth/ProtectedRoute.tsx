2ecfd227d51c246c64ef03418b870c1fd5b024d0
Martin Tsvetanov <martigiant@gmail.com>
2025-11-17 21:04:55 +0100

    Add Netlify SPA redirect

--- src/auth/ProtectedRoute.tsx
+++ src/auth/ProtectedRoute.tsx
@@ -2,22 +2,10 @@
 import { Navigate, Outlet, useLocation } from "react-router-dom";
 import { useAuth } from "./AuthProvider";
 import Navbar from "../components/Navbar/Navbar";
-import { useEffect } from "react";
 
 export function ProtectedRoute() {
     const { loading, isAuthed } = useAuth();
     const loc = useLocation();
-
-    console.log(
-        localStorage.getItem("sb-qakickkytstfcbzpyxvw-auth-token"),
-        `localStorage.getItem("sb-qakickkytstfcbzpyxvw-auth-token")`
-    );
-
-    useEffect(() => {
-        if (!localStorage.getItem("sb-qakickkytstfcbzpyxvw-auth-token")) {
-            <Navigate to="/login" replace state={{ from: loc }} />;
-        }
-    }, [loc]);
 
     if (loading) return null;
 
--- src/auth/PublicOnly.tsx
+++ src/auth/PublicOnly.tsx
@@ -1,18 +1,18 @@
+// src/auth/PublicOnly.tsx
 import { Outlet, Navigate, useLocation } from "react-router-dom";
 import { useAuth } from "./AuthProvider";
-import { useEffect } from "react";
 
 export function PublicOnly() {
     const { loading, isAuthed } = useAuth();
     const loc = useLocation();
-    
-    useEffect(() => {
-        if (!localStorage.getItem("sb-qakickkytstfcbzpyxvw-auth-token")) {
-            <Navigate to="/login" replace state={{ from: loc }} />;
-        }
-    }, [loc]);
 
-    if (loading) return null; // or a spinner
+    if (loading) return null; // or spinner
 
-    return isAuthed ? <Navigate to="/dashboard" /> : <Outlet />;
+    // If user already logged in → redirect to dashboard
+    if (isAuthed) {
+        return <Navigate to="/dashboard" replace state={{ from: loc }} />;
+    }
+
+    // Otherwise render the public routes (login, register)
+    return <Outlet />;
 }
